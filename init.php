2b31303706d414411d2f5cd303b0c636e027dec4
SamZ Ota <sam.otb@hotmail.ca>
2018-05-11 22:56:33 -0400

    first

---
+++ init.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Plugin Name: kalkulator dostav
+Plugin URI: https://github.com/Samz851/kalkulator-dostav.git
+Description: Shipping Calculator. 
+Version: 1.0
+Author: Samer Alotaibi
+Author URI:
+*/
+?>
